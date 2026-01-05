05eb6cc4f1a4ad3338c5a2c793984c0e0b829341
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-01-05 11:33:04 +0530

    all code removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import React from "react";
 import "./App.css";
-import Projects from "./components/Projects";
-import Step from "./components/Step";
-import Test from "./components/Test";
-import Demo from "./components/demo/Demo";
-import HobbitonScroll from "./components/HobbitonScroll";
-import CardCarousel from "./components/CardCarousel";
 
 function App() {
   return (
     <>
-      <div className="folder-icon">
-        {/* <Projects /> */}
-        {/* <Step /> */}
-        {/* <Test /> */}
-        {/* <Demo /> */}
-        {/* <HobbitonScroll /> */}
-        <CardCarousel />
-      </div>
+      <h1>Testing..........</h1>
     </>
   );
 }
