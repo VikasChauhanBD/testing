a1b3b3c2e651894dedad6c059317247217aa8f8b
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-03-12 17:34:46 +0530

    face404 added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import AppShowcase from "./components/appShowcase/AppShowcase";
 import Slider from "./components/Slider/Slider";
 import MzaCarousel from "./components/mzaCarousel/MzaCarousel";
+import Face404 from "./components/face404/Face404";
 
 function App() {
   return (
     <>
       {/* <AppShowcase /> */}
       {/* <Slider /> */}
-      <MzaCarousel />
+      {/* <MzaCarousel /> */}
+      <Face404 />
     </>
   );
 }
