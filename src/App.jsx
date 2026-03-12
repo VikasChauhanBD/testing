f33248026c7e710e40ce1775e2066ea7f7f8843e
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-03-12 16:31:30 +0530

    MzaCarousel added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import AppShowcase from "./components/appShowcase/AppShowcase";
 import Slider from "./components/Slider/Slider";
+import MzaCarousel from "./components/mzaCarousel/MzaCarousel";
 
 function App() {
   return (
     <>
-      <AppShowcase />
+      {/* <AppShowcase /> */}
       {/* <Slider /> */}
+      <MzaCarousel />
     </>
   );
 }
